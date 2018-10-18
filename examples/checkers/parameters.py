b80ac354c5e99bb9544060ac34100a85025c14f8
ccui <ccui@princeton.edu>
2018-10-18 12:28:41 -0400

    rearrange examples

---
+++ examples/checkers/parameters.py
@@ -0,0 +1,54 @@
+
+WORKFLOW='inversion'    # inversion, migration
+SOLVER='specfem2d'      # specfem2d, specfem3d
+SYSTEM='serial'         # serial, pbs, slurm
+OPTIMIZE='LBFGS'        # NLCG, LBFGS
+PREPROCESS='base'       # base
+POSTPROCESS='base'      # base
+
+MISFIT='Waveform'
+MATERIALS='LegacyAcoustic'
+DENSITY='Constant'
+
+
+# WORKFLOW
+BEGIN=1                 # first iteration
+END=5                   # last iteration
+NREC=132                # number of receivers
+NSRC=25                 # number of sources
+SAVEGRADIENT=1          # save gradient how often
+SAVERESIDUALS=1
+
+# PREPROCESSING
+FORMAT='su'             # data file format
+CHANNELS='y'            # data channels
+NORMALIZE=0             # normalize
+BANDPASS=0              # bandpass
+MUTE=0                  # mute direct arrival
+FREQLO=0.               # low frequency corner
+FREQHI=0.               # high frequency corner
+MUTECONST=0.            # mute constant
+MUTESLOPE=0.            # mute slope
+
+
+# POSTPROCESSING
+SMOOTH=20.              # smoothing radius
+SCALE=6.0e6             # scaling factor
+
+
+# OPTIMIZATION
+PRECOND=None            # preconditioner type
+STEPMAX=10              # maximum trial steps
+STEPTHRESH=0.1          # step length safeguard
+
+
+# SOLVER
+NT=4800                 # number of time steps
+DT=0.06                 # time step
+F0=0.084                # dominant frequency
+
+
+# SYSTEM
+NTASK=25                # must satisfy 1 <= NTASK <= NSRC
+NPROC=1                 # processors per task
+
---
+++ examples/checkers/paths.py
@@ -0,0 +1,13 @@
+
+DATA = ''
+
+MODEL_INIT = '$(pwd)/model_init'
+
+MODEL_TRUE = '$(pwd)/model_true'
+
+PRECOND = ''
+
+SPECFEM_DATA = '$(pwd)/specfem2d-d745c542/DATA'
+
+SPECFEM_BIN = '/home/ccui/specfem2d-d745c542/bin'
+
