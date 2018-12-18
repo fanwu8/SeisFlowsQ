5a2557940e61bf7911eb6914575e87120534a407
Congyue Cui <ccui@pop-os.localdomain>
2018-12-17 22:53:51 -0500

    tmp

--- examples/checkers/paths.py
+++ examples/checkers/paths.py
@@ -7,7 +7,9 @@ MODEL_TRUE = '$(pwd)/model_true'
 
 PRECOND = ''
 
-SPECFEM_DATA = '$(pwd)/specfem2d-d745c542/DATA'
+# SPECFEM_DATA = '$(pwd)/specfem2d-d745c542/DATA'
+SPECFEM_DATA = '$(pwd)/specfem2d/DATA'
 
-SPECFEM_BIN = '~/specfem2d-d745c542/bin'
+# SPECFEM_BIN = '~/Documents/specfem2d-d745c542/bin'
+SPECFEM_BIN = '~/Documents/specfem2d/bin'
 
