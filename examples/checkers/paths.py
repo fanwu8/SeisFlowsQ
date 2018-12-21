dd2c9c9edaea9f1f1b6b65d10ff4c03f6ea803b9
ccui <ccui@princeton.edu>
2018-12-20 21:04:14 -0500

    added checkers_legacy for old versions of specfem2d

--- examples/checkers/paths.py
+++ examples/checkers/paths.py
@@ -7,9 +7,7 @@ MODEL_TRUE = '$(pwd)/model_true'
 
 PRECOND = ''
 
-# SPECFEM_DATA = '$(pwd)/specfem2d-d745c542/DATA'
 SPECFEM_DATA = '$(pwd)/specfem2d/DATA'
 
-# SPECFEM_BIN = '~/Documents/specfem2d-d745c542/bin'
-SPECFEM_BIN = '~/Documents/specfem2d/bin'
+SPECFEM_BIN = '$(pwd)/specfem2d/bin'
 
---
+++ examples/checkers_legacy/paths.py
@@ -0,0 +1,13 @@
+
+DATA = ''
+
+MODEL_INIT = '$(pwd)/../checkers/model_init'
+
+MODEL_TRUE = '$(pwd)/../checkers/model_true'
+
+PRECOND = ''
+
+SPECFEM_DATA = '$(pwd)/specfem2d-d745c542/DATA'
+
+SPECFEM_BIN = '$(pwd)/specfem2d-d745c542/bin'
+
