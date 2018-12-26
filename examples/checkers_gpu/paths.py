dc23a915d5282719fe3ea23f64d8bf32e933f3e0
ccui <ccui@princeton.edu>
2018-12-26 17:04:30 -0500

    Added example for GPU mode

---
+++ examples/checkers_gpu/paths.py
@@ -0,0 +1,13 @@
+
+DATA = ''
+
+MODEL_INIT = '$(pwd)/model_init'
+
+MODEL_TRUE = '$(pwd)/model_true'
+
+PRECOND = ''
+
+SPECFEM_DATA = '$(pwd)/specfem2d/DATA'
+
+SPECFEM_BIN = '$(pwd)/specfem2d/bin'
+
