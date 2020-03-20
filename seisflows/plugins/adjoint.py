384280cc5bb9c08b9b2d558ac12f5ad60809fcef
fanwu <fw8@princeton.edu>
2020-03-20 00:23:01 -0400

    amplitude att

--- seisflows/plugins/adjoint.py
+++ seisflows/plugins/adjoint.py
@@ -145,6 +145,19 @@ def Amplitude(syn, obs, nt, dt):
     wadj *= misfit.Amplitude(syn,obs,nt,dt)
     return wadj
 
+def Amplitude_att(syn, obs, nt, dt):
+    wadj0 = Amplitude(syn, obs, nt, dt)
+    tf_adj = fft(wadj0)
+    freq = fftfreq(len(syn), d=dt)
+    freq[0] = 0.001
+    freq_ref = 10
+    freq_mask = _np.ones(len(syn))
+    freq_mask[0:5] = 0
+    wadj = ifft(freq_mask * ((2.0 / _np.pi) * _np.log(abs(freq) / freq_ref) - 1j * _np.sign(freq)) * tf_adj)
+
+    # print(_np.linalg.norm(_np.imag(wadj) / _np.linalg.norm(_np.real(wadj))))
+
+    return wadj.real
 
 
 
